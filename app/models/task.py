5b74876653a253d228c1848a173a3f712107383e
Leidy <leidymarbu@gmail.com>
2024-11-07 00:45:48 -0500

    Wave 1

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,13 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import String, Text, DateTime
 from ..db import db
 
 class Task(db.Model):
+    __tablename__ = 'task'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(nullable=False)   # Title column required
+    description: Mapped[str] = mapped_column(nullable=False)    # Description column required
+    completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True, default=None)
+    is_complete: Mapped[Optional[bool]] = False
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,112 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, make_response, Response
+from app.models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint("task_bp", __name__,url_prefix="/tasks")
+
+@tasks_bp.post("")
+def create_task():
+    request_body = request.get_json() 
+
+    # Check if "title" or "description" keys are missing in the request body
+    if "title" not in request_body or "description" not in request_body:
+        return {"details": "Invalid data"}, 400
+
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body.get("completed_at") # Use .get() since "completed_at" is optional
+    
+    new_task = Task(title=title, description=description, completed_at=completed_at)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        Task.__tablename__: 
+            {
+            "id": new_task.id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": bool(new_task.completed_at) # Convert to boolean for completeness
+        }
+            }
+    return response, 201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    #execute the query statement and retrieve the models
+    query = db.select(Task) #select records from db Model
+    tasks = db.session.scalars(query) #retrieve the records
+    
+    response = []
+    for task in tasks:
+        response.append(
+            {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": bool(task.completed_at)
+            }
+        )
+    return response 
+
+@tasks_bp.get("/<task_id>")
+def get_one_task_by_id(task_id):
+    task = validate_task_id(task_id)
+
+    return {
+        Task.__tablename__: {
+        "id": task.id,
+        "title": task.title,
+        "description": task.description,
+        "is_complete": bool(task.completed_at)
+        }
+    }
+
+def validate_task_id(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        response = {"message": f"Task {task_id} invalid"}
+        abort(make_response(response, 400))
+
+    #execute the query statement and retrieve the models
+    query = db.select(Task).where(Task.id == task_id) #select records with an id = task_id
+    task = db.session.scalar(query) #retrieve only one record task_id
+    
+    if not task:
+        response = {"message": f"Task {task_id} not found"}
+        abort(make_response(response, 404))
+
+    return task
+
+@tasks_bp.put("/<task_id>")
+def update_one_task_by_id(task_id):
+    task = validate_task_id(task_id) #record with id = task_id
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    db.session.commit() #save the changes to db
+
+    return {
+        Task.__tablename__: {
+        "id": task.id,
+        "title": task.title,
+        "description": task.description,
+        "is_complete": bool(task.completed_at)
+        }
+    }
+
+@tasks_bp.delete("/<task_id>")
+def delete_task_by_id(task_id):
+    task = validate_task_id(task_id)
+    
+    #Delete the task
+    db.session.delete(task)
+    db.session.commit()
+    
+    response = {
+        "details": f"Task {task.id} \"{task.title}\" successfully deleted"
+    }
+    return response, 200
